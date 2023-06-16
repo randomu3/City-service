668b03595077a6c6a2c473ec2dfa40a0c02a3dc0
randomu3 <demiz99@mail.ru>
2023-06-16 21:15:44 +0500

    fix github page link

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, "dist"),
+      directory: path.join(__dirname, "src/**/*.html"),
     },
     compress: true,
     port: 9000,
